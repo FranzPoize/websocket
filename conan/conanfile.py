bde97aca0dfb926b4893178b8ee27240a71d10d3
Francois Poizat <francois.poizat@gmail.com>
2021-05-03 15:24:02 +0200

    checking stuff

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -21,7 +21,7 @@ class WebsocketConan(ConanFile):
         "build_tests": False,
     }
 
-    requires = ("boost/1.71.0",)
+    requires = ("boost/1.75.0",)
 
     build_requires = ("cmake_installer/[>=3.16]@conan/stable",)
 
